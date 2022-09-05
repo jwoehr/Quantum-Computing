cd70e27fb578d3c700c3070b3250bc881dd25891
jwoehr <jwoehr@softwoehr.com>
2022-09-05 09:11:56 -0600

    ubuntu install currently broken

--- platforms/linuxone_ubuntu/install_qiskit_linuxone_ubuntu_20.04.sh
+++ platforms/linuxone_ubuntu/install_qiskit_linuxone_ubuntu_20.04.sh
@@ -5,6 +5,10 @@
 # https://linuxone.cloud.marist.edu/#/login
 # Thus, it's dependent on the package configuration used by Community Cloud
 # to provision new instances.
+##############################################################################
+# NOTE -- currently BROKEN -- keeps trying to build SciPy ... even though
+# python3-scipy already installed. Must have bumped a version in Qiskit Terra.
+##############################################################################
 # No guarantee as to usability. Free software, use at your own risk.
 # Installs many packages without asking confirmation.
 # Best used on a fresh instance.
@@ -17,31 +21,32 @@ mkdir Qiskit
 cd Qiskit
 sudo apt-get update
 # We will "pre-approve" the changes `apt` asks if it can make to the system
+yes | sudo apt install python3.9
+yes | sudo apt install python3.9-dev
+yes | sudo apt install python3.9-venv
 yes | sudo apt-get dist-upgrade
 yes | sudo apt install python3-pandas
 yes | sudo apt install python3-jupyter
 yes | sudo apt install python3-notebook
 yes | sudo apt install python3-h5py
-yes | sudo apt install python3.8-venv
-yes | sudo apt install cmake
-yes | sudo apt install ninja-build
+# yes | sudo apt install cmake
+# yes | sudo apt install ninja-build
 yes | sudo apt install python3-scipy
 yes | sudo apt install python-numpy
-yes | sudo apt install python3.8-dev
-yes | sudo apt install lapack
-yes | sudo apt install libblas
-yes | sudo apt install liblas
-yes | sudo apt install liblapack
+# yes | sudo apt install lapack
+# yes | sudo apt install libblas
+# yes | sudo apt install liblas
+# yes | sudo apt install liblapack
 yes | sudo apt install libblas64-dev
 yes | sudo apt install libblas-dev
-yes | sudo apt install libopenblas
+# yes | sudo apt install libopenblas
 yes | sudo apt install libopenblas-dev
 yes | sudo apt install gfortran
 yes | sudo apt install cython3
 yes | sudo apt install python3-sklearn
 yes | sudo apt install libxml2 
 yes | sudo apt install libxslt1-dev
-python3 -m venv qrel_venv --system-site-packages
+python3.9 -m venv qrel_venv --system-site-packages
 . qrel_venv/bin/activate
 pip install -U pip
 pip install wheel
